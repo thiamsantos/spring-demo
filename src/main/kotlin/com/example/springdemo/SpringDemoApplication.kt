80406468e22a113bd64d7eaf74a88702e6b46fe3
Thiago <thia.mdossantos@gmail.com>
2017-12-21 11:01:19 -0200

    enable cors

--- src/main/kotlin/com/example/springdemo/SpringDemoApplication.kt
+++ src/main/kotlin/com/example/springdemo/SpringDemoApplication.kt
@@ -2,9 +2,22 @@ package com.example.springdemo
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class SpringDemoApplication
+class SpringDemoApplication {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurerAdapter() {
+            override fun addCorsMappings(registry: CorsRegistry?) {
+                registry!!.addMapping("/**")
+            }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(SpringDemoApplication::class.java, *args)
